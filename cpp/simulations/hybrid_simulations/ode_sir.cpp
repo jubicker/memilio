d05b48065f17d91f04c363e38aeaad0a6fe74cd9
jubicker <113909589+jubicker@users.noreply.github.com>
2025-08-11 08:26:53 +0200

    add setup

--- cpp/simulations/hybrid_simulations/ode_sir.cpp
+++ cpp/simulations/hybrid_simulations/ode_sir.cpp
@@ -1,53 +1,66 @@
+/*
+* Copyright (C) 2020-2025 MEmilio
+*
+* Authors: Julia Bicker
+*
+* Contact: Martin J. Kuehn <Martin.Kuehn@DLR.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #include "memilio/config.h"
 #include "memilio/data/analyze_result.h"
 #include "ode_sir/infection_state.h"
 #include "ode_sir/model.h"
 #include "ode_sir/parameters.h"
 #include "memilio/compartments/simulation.h"
+#include "setup.cpp"
 
 int main()
 {
 
     mio::set_log_level(mio::LogLevel::debug);
 
-    ScalarType t0   = 0.;
-    ScalarType tmax = 30.;
-    ScalarType dt   = 0.1;
-
-    ScalarType total_population = 1000;
-
-    mio::log_info("Simulating SIR; t={} ... {} with dt = {}.", t0, tmax, dt);
+    mio::log_info("Simulating SIR; t={} ... {} with dt = {}.", setup::t0, setup::tmax, setup::dt);
 
     mio::osir::Model<ScalarType> model(1);
 
-    model.populations[{mio::AgeGroup(0), mio::osir::InfectionState::Infected}]  = 1;
+    model.populations[{mio::AgeGroup(0), mio::osir::InfectionState::Infected}]  = setup::I0;
     model.populations[{mio::AgeGroup(0), mio::osir::InfectionState::Recovered}] = 0;
     model.populations[{mio::AgeGroup(0), mio::osir::InfectionState::Susceptible}] =
-        total_population - model.populations[{mio::AgeGroup(0), mio::osir::InfectionState::Infected}] -
+        setup::total_population - model.populations[{mio::AgeGroup(0), mio::osir::InfectionState::Infected}] -
         model.populations[{mio::AgeGroup(0), mio::osir::InfectionState::Recovered}];
     //Set contact frequency to total population
     mio::ContactMatrixGroup& contact_matrix =
         model.parameters.get<mio::osir::ContactPatterns<ScalarType>>().get_cont_freq_mat();
-    contact_matrix[0].get_baseline().setConstant(total_population);
-    double lambda = 0.002;
-    //rho = lambda*N
-    model.parameters.set<mio::osir::TransmissionProbabilityOnContact<ScalarType>>(lambda);
-    //gamma = 1 / t_I
-    double gamma = 1. / 5.;
-    model.parameters.set<mio::osir::TimeInfected<ScalarType>>(1. / gamma);
+    contact_matrix[0].get_baseline().setConstant(setup::total_population);
+
+    //rho = lambda if contact frequency is total population
+    model.parameters.set<mio::osir::TransmissionProbabilityOnContact<ScalarType>>(setup::lambda);
+    model.parameters.set<mio::osir::TimeInfected<ScalarType>>(1. / setup::gamma);
 
     model.check_constraints();
 
-    auto sir = mio::simulate(t0, tmax, dt, model);
+    auto sir = mio::simulate(setup::t0, setup::tmax, setup::dt, model);
 
     std::string output_file = "/home/bick_ju/Documents/TemporalHybridModel/sim_outputs/ODE/sir.csv";
 
-    int num_steps = static_cast<int>(tmax / dt) + 1;
+    int num_steps = static_cast<int>(setup::tmax / setup::dt) + 1;
 
     std::vector<double> interpolation_tps(num_steps);
 
     for (int i = 0; i < num_steps; ++i) {
-        interpolation_tps[i] = i * dt;
+        interpolation_tps[i] = i * setup::dt;
     }
     auto result = mio::interpolate_simulation_result(sir, interpolation_tps).export_csv(output_file, {"S", "I", "R"});
 }
--- cpp/simulations/hybrid_simulations/smm_sir.cpp
+++ cpp/simulations/hybrid_simulations/smm_sir.cpp
@@ -1,57 +1,71 @@
+/*
+* Copyright (C) 2020-2025 MEmilio
+*
+* Authors: Julia Bicker
+*
+* Contact: Martin J. Kuehn <Martin.Kuehn@DLR.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #include "ode_sir/infection_state.h"
 #include "smm/simulation.h"
 #include "smm/model.h"
 #include "smm/parameters.h"
 #include "memilio/data/analyze_result.h"
 #include "memilio/epidemiology/adoption_rate.h"
+#include "setup.cpp"
 #include <string>
 
 void run_smm_sim(int sim_num)
 {
-    using Model                 = mio::smm::Model<1, mio::osir::InfectionState>;
-    ScalarType total_population = 1000;
-    double num_I                = 1;
+    using Model = mio::smm::Model<1, mio::osir::InfectionState>;
 
     Model model;
-    model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Infected}]    = num_I;
-    model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Recovered}]   = 0;
-    model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Susceptible}] = total_population - num_I;
-
-    //Set lambda and tau
-    double lambda = 0.002;
-    double gamma  = 1. / 5.;
+    model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Infected}]  = setup::I0;
+    model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Recovered}] = 0;
+    model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Susceptible}] =
+        setup::total_population - model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Infected}] -
+        model.populations[{mio::regions::Region(0), mio::osir::InfectionState::Recovered}];
 
     // Second-order adoption rate is lambda * N
     std::vector<mio::AdoptionRate<mio::osir::InfectionState>> adoption_rates;
+    // lambda has to be multiplied with the contact frequency which is the total population
     adoption_rates.push_back({mio::osir::InfectionState::Susceptible,
                               mio::osir::InfectionState::Infected,
                               mio::regions::Region(0),
-                              lambda * total_population,
+                              setup::lambda * setup::total_population,
                               {{mio::osir::InfectionState::Infected, 1.}}});
     adoption_rates.push_back({mio::osir::InfectionState::Infected,
                               mio::osir::InfectionState::Recovered,
                               mio::regions::Region(0),
-                              gamma,
+                              setup::gamma,
                               {}});
 
     model.parameters.get<mio::smm::AdoptionRates<mio::osir::InfectionState>>() = adoption_rates;
 
-    ScalarType t0   = 0.;
-    ScalarType tmax = 30.;
-    ScalarType dt   = 0.1;
-
-    auto sim = mio::smm::Simulation(model, t0, dt);
-    sim.advance(tmax);
+    auto sim = mio::smm::Simulation(model, setup::t0, setup::dt);
+    sim.advance(setup::tmax);
 
     std::string output_file =
         "/home/bick_ju/Documents/TemporalHybridModel/sim_outputs/SMM/" + std::to_string(sim_num) + "_comps.csv";
 
-    int num_steps = static_cast<int>(tmax / dt) + 1;
+    int num_steps = static_cast<int>(setup::tmax / setup::dt) + 1;
 
     std::vector<double> interpolation_tps(num_steps);
 
     for (int i = 0; i < num_steps; ++i) {
-        interpolation_tps[i] = i * dt;
+        interpolation_tps[i] = i * setup::dt;
     }
     auto result = mio::interpolate_simulation_result(sim.get_result(), interpolation_tps)
                       .export_csv(output_file, {"S", "I", "R"});
