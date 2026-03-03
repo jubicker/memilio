1647957e2aed2d62615ddb4208db0f547a757841
jubicker <julia.bicker@dlr.de>
2026-03-03 09:52:59 +0100

    plot scripts

--- cpp/simulations/hybrid_simulations/sir_metapop/python/plot_comparison_hybrid_smm.py
+++ cpp/simulations/hybrid_simulations/sir_metapop/python/plot_comparison_hybrid_smm.py
@@ -26,10 +26,10 @@ def plot_mean_error(smm_dir, hybrid_dir, ode_dir,save_dir, switching_thresholds,
         ax.plot(ode_values.Time, error, label = label, color = color_ode[r], linewidth=linewidth)
     
         for i, st in enumerate(switching_thresholds):
-            values = pd.read_csv(hybrid_dir + f"/switch_value_{st}/means.csv")
+            values = pd.read_csv(hybrid_dir + f"/{st}/means.csv")
             error = (smm_values.iloc[:-1, 1 + comp_index + r * len(compartment_names)] - values.iloc[:, 1 + comp_index + r * len(compartment_names)]).abs()
             summed_errors[r].append(error.sum())
-            label = f"{float(st) * 100:.3f}%"
+            label = f"{st}"
             if(num_regions > 1):
                 label += f" Region {r}" 
             ax.plot(values.Time, error, label = label, color = colors_hybrid[r][i], linewidth=linewidth)
@@ -59,7 +59,7 @@ def plot_mean_error(smm_dir, hybrid_dir, ode_dir,save_dir, switching_thresholds,
     ax.set_xticks(x)
     ax.set_xticklabels(["" for _ in range(len(x))])
     ax.set_ylabel("Summed error")
-    ax.set_yscale("log")
+    #ax.set_yscale("log")
     fig.tight_layout()
     fig.savefig(save_dir + f"/mean_sum_err_{comp}.png", dpi=dpi)
     plt.close()
@@ -85,10 +85,10 @@ def plot_variance_error(smm_dir, hybrid_dir, ode_dir,save_dir, switching_thresho
         ax.plot(ode_values.Time, error, label = "ODE", color = color_ode[r])
 
         for i, st in enumerate(switching_thresholds):
-            values = pd.read_csv(hybrid_dir + f"/switch_value_{st}/moments.csv")
+            values = pd.read_csv(hybrid_dir + f"/{st}/moments.csv")
             error = (smm_values[col_name].iloc[:-1] - values[col_name].iloc[:]).abs()
             summed_errors[r].append(error.sum())
-            label = f"{float(st) * 100:.3f}%"
+            label = f"{st}"
             if(num_regions > 1):
                 label += f" Region {r}" 
             ax.plot(values.Time, error, label = label, color = colors_hybrid[r][i])
@@ -137,12 +137,12 @@ def plot_runtimes(smm_dir, hybrid_dir, ode_dir, save_dir, switching_thresholds, 
     labels.append("ODE")
     # Hybrid
     for st in switching_thresholds:
-        runtime = pd.read_csv(hybrid_dir + f"/switch_value_{st}/runtimes.csv")
+        runtime = pd.read_csv(hybrid_dir + f"/{st}/runtimes.csv")
         mean_runtimes.append(runtime['Runtime'].mean())
         max_runtimes.append(runtime['Runtime'].max())
-        runtime = pd.read_csv(hybrid_dir + f"/switch_value_{st}/total_time.csv")
+        runtime = pd.read_csv(hybrid_dir + f"/{st}/total_time.csv")
         total_runtime.append(runtime['Runtime'].iloc[0])
-        labels.append(f"{float(st) * 100:.4f}%")
+        labels.append(f"{st}")
         
     # SMM    
     runtime  = pd.read_csv(smm_dir + "/runtimes.csv")
@@ -237,11 +237,11 @@ def plot_mean_var_ts(dir, name_mean, name_var, switching_thresholds, comp_index,
             fig.savefig(f"{save_dir}/mean_var_{compartment_names[comp]}_{st}_r{r}.png", dpi=dpi)
             plt.close(fig)
         
-def plot_mean_ts(dir, name_mean, switching_thresholds, comp_index, num_regions, figsize, tmax, tmin, name2_mean="", dir_smm="", color_smm=""):
+def plot_mean_ts(dir, name_mean, compare_values, comp_index, num_regions, figsize, tmax, tmin, name2_mean="", dir_smm="", color_smm="", closure_order = "closure_order_3"):
     comp = list(compartment_colors.keys())[comp_index]
     
     for r in range(num_regions):
-        for st in switching_thresholds:
+        for st in compare_values:
             fig, ax = plt.subplots(figsize=figsize)
             
             if(dir_smm) != "":
@@ -250,13 +250,13 @@ def plot_mean_ts(dir, name_mean, switching_thresholds, comp_index, num_regions, 
                 mean = mean[mean['Time'] >= tmin]
                 ax.plot(mean.Time, mean.iloc[:, 1 + comp_index + r * len(compartment_names)], color=color_smm, label = r"$\mu$ SMM")
             
-            mean = pd.read_csv(dir + f"/switch_value_{st}/" + name_mean + ".csv")
+            mean = pd.read_csv(dir + f"/{st}/{closure_order}/" + name_mean + ".csv")
             mean = mean[mean['Time'] <= tmax]
             mean = mean[mean['Time'] >= tmin]
             ax.plot(mean.Time, mean.iloc[:, 1 + comp_index + r * len(compartment_names)], color=compartment_colors[comp][0], label = r"$\mu$")
             
             if(name2_mean != ""):
-                mean = pd.read_csv(dir + f"/switch_value_{st}/" + name2_mean + ".csv")
+                mean = pd.read_csv(dir + f"/{st}/{closure_order}/" + name2_mean + ".csv")
                 mean = mean[mean['Time'] <= tmax]
                 mean = mean[mean['Time'] >= tmin]
                 ax.plot(mean.Time.iloc[1:], mean.iloc[1:, 1 + comp_index + r * len(compartment_names)], color=compartment_colors[comp][0])
@@ -274,7 +274,7 @@ def plot_mean_ts(dir, name_mean, switching_thresholds, comp_index, num_regions, 
             fig.savefig(f"{save_dir}/mean_{compartment_names[comp]}_{st}_r{r}.png", dpi=dpi)
             plt.close(fig)
 
-def plot_var_ts(dir, name_var, switching_thresholds, comp_index, num_regions, figsize, name2_var="", dir_smm="", color_smm=""):
+def plot_var_ts(dir, name_var, compare_values, comp_index, num_regions, figsize, name2_var="", dir_smm="", color_smm="", closure_order = "closure_order_3"):
     comp = list(compartment_colors.keys())[comp_index]
     for r in range(num_regions):
         moment_index = [0 for _ in range(len(compartment_names)*num_regions)]
@@ -282,18 +282,18 @@ def plot_var_ts(dir, name_var, switching_thresholds, comp_index, num_regions, fi
         col_name = f"M"
         for m in moment_index:
             col_name += f"{m}"
-        for st in switching_thresholds:
+        for st in compare_values:
             fig, ax = plt.subplots(figsize=figsize)
             
             if(dir_smm) != "":
                 var = pd.read_csv(dir_smm + f"/moments.csv")
                 ax.plot(var.Time, var[col_name].iloc[:], color=color_smm, label = r"$\sigma^2$ SMM")
             
-            var = pd.read_csv(dir + f"/switch_value_{st}/" + name_var + ".csv")
+            var = pd.read_csv(dir + f"/{st}/{closure_order}/" + name_var + ".csv")
             ax.plot(var.Time, var[col_name].iloc[:], color = compartment_colors[comp][0], label = r"$\sigma^2$")
             
             if(name2_var != ""):
-                var = pd.read_csv(dir + f"/switch_value_{st}/" + name2_var + ".csv")
+                var = pd.read_csv(dir + f"/{st}/{closure_order}/" + name2_var + ".csv")
                 ax.plot(var.Time.iloc[1:], var[col_name].iloc[1:], color = compartment_colors[comp][0])
                 # Add line at swithcing tp
                 switch_tp = var.Time.iloc[1]
@@ -310,37 +310,71 @@ def plot_var_ts(dir, name_var, switching_thresholds, comp_index, num_regions, fi
             fig.savefig(f"{save_dir}/var_{compartment_names[comp]}_{st}_r{r}.png", dpi=dpi)
             plt.close(fig)
      
+def plot_switching_times(hybrid_dir, save_dir, comparing_values, colors_hybrid, figsize):
+    tps = []
+    labels = []
+
+    for i, st in enumerate(comparing_values):
+        values = pd.read_csv(hybrid_dir + f"/{st}/means_smm.csv")
+        tp =  values.iloc[-1, 0]
+        tps.append(tp)
+        labels.append(st)
+
+    
+    # Save summed error
+    fig, ax = plt.subplots(figsize=(figsize[0], figsize[1]*0.8))
+    x = range(len(comparing_values))
+    width = 0.8 / num_regions
+    bar_colors = colors_hybrid[0]
+    ax.bar([i - width/2 for i in x], tps, width=width, color =  bar_colors)
+    ax.set_xticks(x)
+    ax.set_xticklabels(["" for _ in range(len(x))])
+    ax.set_ylabel("Switching tp")
+    fig.tight_layout()
+    fig.savefig(save_dir + f"/switching_tps.png", dpi=dpi)
+    plt.close()
+    
+    # Save legend
+    handles, labels_fig = ax.get_legend_handles_labels()
+    fig_leg = plt.figure(figsize=figsize)                   
+    fig_leg.legend(handles, labels, loc='center')      
+    fig_leg.tight_layout()
+    fig_leg.savefig(save_dir + f"/legend_switch_tps.png", dpi=dpi, bbox_inches='tight', transparent=True)
+    plt.close(fig_leg)
 
 if __name__ == "__main__":
     figsize = (4, 3)
     dir = "V:/bick_ju/TemporalHybrid"
     save_dir = "H:/Documents/TemporalHybridModel"
     hybrid_model = "Hybrid2"
-    config = "config_2r1"
-    num_regions = 2
-    switching_values = ["0.100000"]#["0.000010", "0.000100", "0.001000", "0.010000", "0.100000", "0.300000"]
+    config = "config1_1r_I0_100"
+    num_regions = 1
+    compare_values = ["var_gradient/truncation/closure_order_3", "mean_threshold/0.001000/truncation/closure_order_3", "mean_threshold/0.010000/truncation/closure_order_3", "mean_threshold/0.100000/truncation/closure_order_3", "mean_threshold/0.300000/truncation/closure_order_3"]
     colors_hybrid = [[colors['dark blue'], colors['middle blue'], colors['light blue'], colors['teal'], colors['light teal'], colors['dark green'], colors['middle green'], colors['light green']], [colors['purple'], colors['rose'], colors['red'], colors['dark red'], colors['brown']]]
     color_ode = [colors['black'], colors['dark grey']]
     color_smm = colors['dark grey']
     tmin = 0
     tmax = 300
+    condition_name = ""
     
     smm_dir = f"{dir}/SMM/{config}"
-    hybrid_dir = f"{dir}/{hybrid_model}/{config}"
-    ode_dir = f"{dir}/Moments/{config}/closure_order_2/0.000000"
-    save_dir = f"{save_dir}/{hybrid_model}/{config}"
+    hybrid_dir = f"{dir}/{hybrid_model}/{config}/{condition_name}"
+    ode_dir = f"{dir}/Moments/{config}/truncation/closure_order_3/0.000000"
+    save_dir = f"{save_dir}/{hybrid_model}/{config}/{condition_name}"
     os.makedirs(save_dir, exist_ok=True)
     
     comp_index = 1
     
-    plot_mean_ts(hybrid_dir, "means_smm", ["1.000000"], comp_index, num_regions,figsize, tmax, tmin)
-    plot_var_ts(hybrid_dir, "moments_smm", ["1.000000"], comp_index, num_regions, figsize)
-    plot_mean_var_ts(hybrid_dir, "means_smm", "moments_smm", ["1.000000"], comp_index, num_regions, figsize)
-    plot_mean_ts(hybrid_dir, "means_smm", switching_values, comp_index, num_regions, figsize, tmax, tmin, "expected_values_moments", smm_dir, color_smm)
-    plot_var_ts(hybrid_dir, "moments_smm", switching_values, comp_index, num_regions, figsize, "moments_moments", smm_dir, color_smm)
-    plot_mean_var_ts(hybrid_dir, "means_smm", "moments_smm", switching_values, comp_index, num_regions, figsize, "expected_values_moments", "moments_moments", smm_dir, color_smm)
+    # plot_mean_ts(hybrid_dir, "means_smm", ["1.000000"], comp_index, num_regions,figsize, tmax, tmin)
+    # plot_var_ts(hybrid_dir, "moments_smm", ["1.000000"], comp_index, num_regions, figsize)
+    # plot_mean_var_ts(hybrid_dir, "means_smm", "moments_smm", ["1.000000"], comp_index, num_regions, figsize)
+    # plot_mean_ts(hybrid_dir, "means_smm", compare_values, comp_index, num_regions, figsize, tmax, tmin, "expected_values_moments", smm_dir, color_smm)
+    # plot_var_ts(hybrid_dir, "moments_smm", compare_values, comp_index, num_regions, figsize, "moments_moments", smm_dir, color_smm)
+    # plot_mean_var_ts(hybrid_dir, "means_smm", "moments_smm", switching_values, comp_index, num_regions, figsize, "expected_values_moments", "moments_moments", smm_dir, color_smm)
     
     #for comp_index in range(len(compartment_names)):
-    plot_mean_error(smm_dir, hybrid_dir, ode_dir, save_dir, switching_values, colors_hybrid, color_ode, comp_index, num_regions, figsize)
-    plot_variance_error(smm_dir, hybrid_dir, ode_dir, save_dir, switching_values, colors_hybrid, color_ode, comp_index, num_regions,figsize)
-    plot_runtimes(smm_dir, hybrid_dir, ode_dir, save_dir, switching_values, colors_hybrid, color_ode, color_smm, figsize)
+    # plot_mean_error(smm_dir, hybrid_dir, ode_dir, save_dir, compare_values, colors_hybrid, color_ode, comp_index, num_regions, figsize)
+    # plot_variance_error(smm_dir, hybrid_dir, ode_dir, save_dir, compare_values, colors_hybrid, color_ode, comp_index, num_regions,figsize)
+    # plot_runtimes(smm_dir, hybrid_dir, ode_dir, save_dir, compare_values, colors_hybrid, color_ode, color_smm, figsize)
+    
+    plot_switching_times(hybrid_dir, save_dir, compare_values, colors_hybrid, figsize)
---
+++ cpp/simulations/hybrid_simulations/sir_metapop/python/plot_moments.py
@@ -0,0 +1,192 @@
+from settings import *
+import os
+
+def plot_mean_ts(dir, save_dir, closures, comp_index, num_regions, figsize, closure_colors, closure_order, init_tp, dir_smm="", color_smm=""):
+    comp = list(compartment_colors.keys())[comp_index]
+    
+    for r in range(num_regions):
+        fig, ax = plt.subplots(figsize=figsize)
+        # Plot smm mean
+        if(dir_smm) != "":
+            mean = pd.read_csv(dir_smm + f"/means.csv")
+            ax.plot(mean.Time, mean.iloc[:, 1 + comp_index + r * len(compartment_names)], color=color_smm, label = "SMM")
+        for c in closures:            
+            mean = pd.read_csv(dir + f"/{c}/closure_order_{closure_order}/{init_tp}/" + "means.csv")
+            ax.plot(mean.Time, mean.iloc[:, 1 + comp_index + r * len(compartment_names)], color=closure_colors[c], label = f"{c}")
+                
+        ax.set_xlabel("Time [days]")
+        ax.set_ylabel(f"{compartment_names[comp]} [#]")
+        if(dir_smm!=""):
+            mean_smm = pd.read_csv(dir_smm + f"/means.csv")
+            ax.set_ylim(-0.5*np.max(mean_smm.iloc[1:, 1 + comp_index + r * len(compartment_names)]), 2*np.max(mean_smm.iloc[1:, 1 + comp_index]))
+        ax.legend()
+        fig.tight_layout()
+        fig.savefig(f"{save_dir}/mean_{compartment_names[comp]}_r{r}.png", dpi=dpi)
+        plt.close(fig)
+        
+def plot_var_ts(dir, save_dir, closures, comp_index, num_regions, figsize, closure_colors, closure_order, init_tp, dir_smm="", color_smm=""):
+    comp = list(compartment_colors.keys())[comp_index]
+    
+    for r in range(num_regions):
+        moment_index = [0 for _ in range(len(compartment_names)*num_regions)]
+        moment_index[comp_index + r * len(compartment_names)] = 2  # Variance moments: M200, M020, M002
+        col_name = f"M"
+        for m in moment_index:
+            col_name += f"{m}"
+        fig, ax = plt.subplots(figsize=figsize)
+        # Plot smm mean
+        if(dir_smm) != "":
+            var = pd.read_csv(dir_smm + f"/moments.csv")
+            ax.plot(var.Time, var[col_name], color=color_smm, label = "SMM")
+        for c in closures:           
+            var = pd.read_csv(dir + f"/{c}/closure_order_{closure_order}/{init_tp}/" + "moments.csv")
+            ax.plot(var.Time, var[col_name], color=closure_colors[c], label = f"{c}")
+                
+        ax.set_xlabel("Time [days]")
+        ax.set_ylabel(f"{compartment_names[comp]} [#]")
+        if(dir_smm!=""):
+            var_smm = pd.read_csv(dir_smm + f"/moments.csv")
+            ax.set_ylim(-0.5*np.max(var_smm[col_name].iloc[:]), 1.5*np.max(var_smm[col_name].iloc[:]))
+        ax.legend()
+        fig.tight_layout()
+        fig.savefig(f"{save_dir}/var_{compartment_names[comp]}_r{r}.png", dpi=dpi)
+        plt.close(fig)
+        
+def plot_mean_error(smm_dir, hybrid_dir, save_dir, closure_names, colors_hybrid, comp_index, num_regions, figsize, closure_order = "closure_order_3"):
+    comp = list(compartment_colors.keys())[comp_index]
+    fig, ax = plt.subplots(figsize=figsize)
+    
+    smm_values = pd.read_csv(smm_dir + "/means.csv")
+    
+    summed_errors = {r: [] for r in range(num_regions)}
+    linewidth = 1
+    
+    for r in range(num_regions):
+    
+        for i, st in enumerate(closure_names):
+            
+            values = pd.read_csv(hybrid_dir + f"/{st}/{closure_order}/means.csv")
+            error = (smm_values.iloc[:-1, 1 + comp_index + r * len(compartment_names)] - values.iloc[:, 1 + comp_index + r * len(compartment_names)]).abs()
+            summed_errors[r].append(error.sum())
+            label = f"{st}"
+            if(num_regions > 1):
+                label += f" Region {r}" 
+            ax.plot(values.Time, error, label = label, color = colors_hybrid[r][i], linewidth=linewidth)
+        
+        ax.set_xlabel("Time [days]")
+        ax.set_ylabel(r"|$\mu_t - \hat{\mu}_t$|")
+        ax.set_yscale("log")
+        fig.tight_layout()
+        fig.savefig(save_dir + f"/mean_error_ts_{comp}_r{r}", dpi=dpi)
+    
+    # Save legend
+    handles, labels = ax.get_legend_handles_labels()
+    fig_leg = plt.figure(figsize=figsize)                   
+    fig_leg.legend(handles, labels, loc='center')      
+    fig_leg.tight_layout()
+    fig_leg.savefig(save_dir + f"/legend_{comp}.png", dpi=dpi, bbox_inches='tight', transparent=True)
+    plt.close(fig_leg)
+    
+    # Save summed error
+    fig, ax = plt.subplots(figsize=(figsize[0], figsize[1]*0.8))
+    x = range(len(closure_names))
+    width = 0.8 / num_regions
+    for r in range(num_regions):
+        summed_errors_region = summed_errors[r]
+        bar_colors = colors_hybrid[r]
+        ax.bar([i + (r-1) * width/2 + r * width/2 for i in x], summed_errors_region, width=width, color =  bar_colors)
+    ax.set_xticks(x)
+    ax.set_xticklabels(["" for _ in range(len(x))])
+    ax.set_ylabel(r"$\sum_{t}$|$\mu_t - \hat{\mu}_t$|")
+    ax.set_yscale("log")
+    fig.tight_layout()
+    fig.savefig(save_dir + f"/mean_sum_err_{comp}.png", dpi=dpi)
+    plt.close()
+        
+def plot_var_error(smm_dir, hybrid_dir, save_dir, closure_names, colors_hybrid, comp_index, num_regions, figsize, closure_order = "closure_order_3"):
+    comp = list(compartment_colors.keys())[comp_index]
+    fig, ax = plt.subplots(figsize=figsize)
+    
+    smm_values = pd.read_csv(smm_dir + "/moments.csv")
+    
+    summed_errors = {r: [] for r in range(num_regions)}
+    linewidth = 1
+    
+    for r in range(num_regions):
+        moment_index = [0 for _ in range(len(compartment_names)*num_regions)]
+        moment_index[comp_index + r * len(compartment_names)] = 2  # Variance moments: M200, M020, M002
+        col_name = f"M"
+        for m in moment_index:
+            col_name += f"{m}"
+        for i, st in enumerate(closure_names):
+            
+            values = pd.read_csv(hybrid_dir + f"/{st}/{closure_order}/moments.csv")
+            error = (smm_values[col_name].iloc[:-1] - values[col_name].iloc[:]).abs()
+            summed_errors[r].append(error.sum())
+            label = f"{st}"
+            if(num_regions > 1):
+                label += f" Region {r}" 
+            ax.plot(values.Time, error, label = label, color = colors_hybrid[r][i], linewidth=linewidth)
+        
+        ax.set_xlabel("Time [days]")
+        ax.set_ylabel(r"|$\sigma^2_t - \hat{\sigma^2}_t$|")
+        ax.set_yscale("log")
+        fig.tight_layout()
+        fig.savefig(save_dir + f"/var_error_ts_{comp}_r{r}", dpi=dpi)
+    
+    # Save legend
+    handles, labels = ax.get_legend_handles_labels()
+    fig_leg = plt.figure(figsize=figsize)                   
+    fig_leg.legend(handles, labels, loc='center')      
+    fig_leg.tight_layout()
+    fig_leg.savefig(save_dir + f"/legend_{comp}.png", dpi=dpi, bbox_inches='tight', transparent=True)
+    plt.close(fig_leg)
+    
+    # Save summed error
+    fig, ax = plt.subplots(figsize=(figsize[0], figsize[1]*0.8))
+    x = range(len(closure_names))
+    width = 0.8 / num_regions
+    for r in range(num_regions):
+        summed_errors_region = summed_errors[r]
+        bar_colors = colors_hybrid[r]
+        ax.bar([i + (r-1) * width/2 + r * width/2 for i in x], summed_errors_region, width=width, color =  bar_colors)
+    ax.set_xticks(x)
+    ax.set_xticklabels(["" for _ in range(len(x))])
+    ax.set_ylabel(r"$\sum_{t}$|$\sigma^2_t - \hat{\sigma^2}_t$|")
+    ax.set_yscale("log")
+    fig.tight_layout()
+    fig.savefig(save_dir + f"/var_sum_err_{comp}.png", dpi=dpi)
+    plt.close()
+
+if __name__ == "__main__":
+    figsize = (4, 3)
+    dir = "V:/bick_ju/TemporalHybrid"
+    save_dir = "H:/Documents/TemporalHybridModel"
+    config = "config1_1r_I0_1"
+    closure_order = 3
+    num_regions = 1
+    closures = ["truncation", "pair_approx", "lognorm"]
+    start_tp = "30.000000"
+    colors_closures = {"truncation": colors["dark blue"], "pair_approx": colors["teal"], "lognorm": colors["dark green"]}
+    color_ode = [colors['black'], colors['dark grey']]
+    color_smm = colors['dark grey']
+    condition_name = "mean_threshold/0.000000"
+    hybrid_model = "Hybrid2"
+    compare_values = ["truncation", "pair_approx", "lognorm"]
+    colors_hybrid = [[colors['dark blue'], colors['middle blue'], colors['light blue'], colors['teal'], colors['light teal'], colors['dark green'], colors['middle green'], colors['light green']], [colors['purple'], colors['rose'], colors['red'], colors['dark red'], colors['brown']]]
+    
+    smm_dir = f"{dir}/SMM/{config}"
+    moment_dir = f"{dir}/Moments/{config}"
+    ode_dir = f"{dir}/Moments/{config}"
+    #save_dir = f"{save_dir}/Moments/{config}/closure_order_{closure_order}/{start_tp}"
+    save_dir = f"{save_dir}/{hybrid_model}/{config}/{condition_name}"
+    hybrid_dir = f"{dir}/{hybrid_model}/{config}/{condition_name}"
+    os.makedirs(save_dir, exist_ok=True)
+    
+    comp_index = 1
+    
+    # plot_mean_ts(moment_dir, save_dir, closures, comp_index, num_regions, figsize, colors_closures, closure_order, start_tp, smm_dir, color_smm)
+    # plot_var_ts(moment_dir, save_dir, closures, comp_index, num_regions, figsize, colors_closures, closure_order, start_tp, smm_dir, color_smm)
+    
+    plot_mean_error(smm_dir, hybrid_dir, save_dir, compare_values, colors_hybrid, comp_index, num_regions, figsize)
+    plot_var_error(smm_dir, hybrid_dir, save_dir, compare_values, colors_hybrid, comp_index, num_regions, figsize)
